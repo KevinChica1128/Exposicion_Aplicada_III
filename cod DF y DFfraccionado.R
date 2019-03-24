c81d9fd741bae5b19dd9244c51b8d51bf2d409c4
KevinChica1128 <kevin.chica@correounivalle.edu.co>
2019-03-24 11:24:45 -0500

    informe

--- cod DF y DFfraccionado.R
+++ cod DF y DFfraccionado.R
@@ -66,6 +66,56 @@ potDgn$design
 mod2<-lm(Vida~Material+Temperatura+Material:Temperatura, data=potDgn$design)
 anova(mod2)
 
+#Ejemplo 2
+A<-c(rep(c("-","+"),8))
+B<-c(rep(c("-","-","+","+"),4))
+C<-c(rep(c(rep("-",4),rep("+",4)),2))
+D<-c(rep("-",8),rep("+",8))
+filtracion<-c(45,71,48,65,68,60,80,65,43,100,45,104,75,86,70,96)
+datos2=data.frame(A,B,C,D,filtracion)
+str(datos2)
+head(datos2)
+mod3<-lm(filtracion~A+C+D+A:C+A:D+C:D+A:C:D, data=datos2)
+anova(mod3)
 
+library(FrF2)
+Tabla <- FrF2(nruns = 16, 
+             nfactors = 4, 
+             factor.names = list(A=c("-","+"), 
+                                 B=c("-","+"), 
+                                 C=c("-","+"),
+                                 D=c("-","+")),
+             replications = 1, randomize = F)
+Tabla <- add.response(design = Tabla, response = filtracion)
+Tabla
+#Análisis método de Daniel
+DanielPlot(Tabla)
 
+#Gráfico efectos principales
+MEPlot(Tabla, lwd = 2)
+abline(h=0, col="red")
 
+## Gráficas de Interacciones
+IAPlot(Tabla, lwd = 2)
+
+# Gráfica de interacción triple
+filtracion1<-filtracion[1:8]
+A1<-A[1:8]
+B1<-B[1:8]
+C1<-C[1:8]
+filtracion2<-filtracion[9:16]
+A2<-A[9:16]
+B2<-B[9:16]
+C2<-C[9:16]
+x11()
+par(mfrow=c(1,2))
+cubePlot(obj = filtracion1, 
+         eff1 = A1, 
+         eff2 = B1, 
+         eff3 = C1, 
+         main = " Gráfica de interacción triple con factor D bajo")
+cubePlot(obj = filtracion2, 
+         eff1 = A2, 
+         eff2 = B2, 
+         eff3 = C2, 
+         main = " Gráfica de interacción triple con factor D alto",round = -1)
